596cf9c1c390005e60f334ebd3fe7b6c04afff64
Rishab Chavadar <rchavadar@gmail.com>
2024-09-26 21:36:30 +0530

    Add files via upload

---
+++ Python/Oneplus.py
@@ -0,0 +1,42 @@
+import turtle
+t = turtle.Turtle()
+t.pensize(10)
+t.color("#07080b" , "White")
+t.fd(100)
+t.bk(100)
+t.lt(90)
+t.fd(100)
+t.rt(90)
+t.fd(50)
+t.penup()
+t.fd(25)
+t.pendown()
+t.fd(50)
+t.penup()
+t.bk(25)
+t.lt(90)
+t.pendown()
+t.fd(25)
+t.bk(50)
+t.lt(180)
+t.penup()
+t.fd(20)
+t.pendown()
+t.fd(55) 
+t.penup()
+t.rt(90)
+t.fd(50)
+t.rt(90)
+t.fd(20)
+t.pendown()
+t.fd(60)
+t.lt(180)
+t.circle(-20 , 90)
+
+t.hideturtle()
+
+
+
+
+
+turtle.done()
